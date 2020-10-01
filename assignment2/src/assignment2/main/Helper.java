387bd725d060909597b0d7367270547c20e4d29a
Sebastian Vittersø <sebastian.vitters@gmail.com>
2020-10-01 12:09:23 +0200

    Update generate.mtl and Helper.java

--- assignment2/src/assignment2/main/Helper.java
+++ assignment2/src/assignment2/main/Helper.java
@@ -1,8 +1,43 @@
 package assignment2.main;
 
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.util.EObjectContainmentEList;
+import org.eclipse.ocl.ecore.OrderedSetType;
+
+import studyplans.Course;
+import studyplans.Field;
+import studyplans.OpenSlot;
+import studyplans.Semester;
+import studyplans.StudyplansPackage;
+import studyplans.University;
+
 public class Helper {
 	
 	public String repeatString(String str, int num) {
 		return str.repeat(num);
+	}
+	
+	public EList<Course> getOpenSlotCourses(Semester semester) {
+		EList<OpenSlot> slots = semester.getOpenSlots();
+
+		EList<Field> acceptedFields = new EObjectContainmentEList<Field>(Field.class, null, StudyplansPackage.FIELD);
+		
+		for (OpenSlot openSlot : slots) {
+			acceptedFields.addAll(openSlot.getFields());
+		}
+		
+		
+		EList<Course> courses = new EObjectContainmentEList<Course>(Course.class, null, StudyplansPackage.COURSE);
+		
+		EList<Course> allCourses = semester.getInProgramme().getInUniversity().getCourses();
+		
+		for (Course course : allCourses) {
+			if(acceptedFields.contains(course.getField())) {
+				courses.add(course);
+			}
+		}
+		
+		
+		return courses;
 	}
 }
