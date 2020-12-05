c7a70d7ac1777103094ca0e75263cb0fc4761f87
Ingebrigt Nygård <ingi.n97@gmail.com>
2020-12-05 15:11:22 +0100

    hmm

--- project/atb.diagram.v2/src/atb/diagram/v2/Services.java
+++ project/atb.diagram.v2/src/atb/diagram/v2/Services.java
@@ -10,9 +10,8 @@ public class Services {
     /**
     * See http://help.eclipse.org/neon/index.jsp?topic=%2Forg.eclipse.sirius.doc%2Fdoc%2Findex.html&cp=24 for documentation on how to write service methods.
     */
-    public EObject myService(EObject self, String arg) {
-       // TODO Auto-generated code
-      return self;
+    public int myService() {
+      return 0;
     }
     
 }
