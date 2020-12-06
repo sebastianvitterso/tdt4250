30a775fa9717e18e94499c15b7329434cff180f0
Ingebrigt Nygård <ingebrin@stud.ntnu.no>
2020-12-06 15:23:44 +0100

    Added test service, not quite working

--- project/atb.diagram.v2/src/atb/diagram/v2/Services.java
+++ project/atb.diagram.v2/src/atb/diagram/v2/Services.java
@@ -1,6 +1,10 @@
 package atb.diagram.v2;
 
+
 import org.eclipse.emf.ecore.EObject;
+
+import import_.Quay;
+
 
 /**
  * The services class used by VSM.
@@ -10,8 +14,9 @@ public class Services {
     /**
     * See http://help.eclipse.org/neon/index.jsp?topic=%2Forg.eclipse.sirius.doc%2Fdoc%2Findex.html&cp=24 for documentation on how to write service methods.
     */
-    public int myService() {
-      return 0;
+    public String myService(EObject quay) {
+//      return ((Quay) quay).getName();
+      return "test";
     }
     
 }
