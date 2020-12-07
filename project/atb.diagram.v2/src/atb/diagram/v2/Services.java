b034e96c512e74a258f0f05beb8e46fd137f993d
Ingebrigt Nygård <ingebrin@stud.ntnu.no>
2020-12-07 20:40:45 +0100

    Added registration of name and number

--- project/atb.diagram.v2/src/atb/diagram/v2/Services.java
+++ project/atb.diagram.v2/src/atb/diagram/v2/Services.java
@@ -10,13 +10,15 @@ import import_.Quay;
  * The services class used by VSM.
  */
 public class Services {
+	
     
     /**
     * See http://help.eclipse.org/neon/index.jsp?topic=%2Forg.eclipse.sirius.doc%2Fdoc%2Findex.html&cp=24 for documentation on how to write service methods.
     */
-    public String myService(EObject quay) {
-//      return ((Quay) quay).getName();
+    public String myService(EObject self) {
+//      Quay quay = (Quay) self;
       return "test";
+//      return quay.toString();
     }
     
 }
